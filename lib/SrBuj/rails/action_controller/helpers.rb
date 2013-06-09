cd5c71ae15ac5d12bf70a5dec9644303d4c9c482
gagoar <xeroice@gmail.com>
2013-06-08 21:50:38 -0300

    simplyfing the js_notify method

--- lib/SrBuj/rails/action_controller/helpers.rb
+++ lib/SrBuj/rails/action_controller/helpers.rb
@@ -1,7 +1,12 @@
 module SrBuj
   module ActionController
     module Helpers
-
+      HEADERS = { message: 'X-SRBUJ-MSG',
+                  type: 'X-SRBUJ-TYPE',
+                  side: 'X-SRBUJ-SIDE',
+                  position: 'X-SRBUJ-POS',
+                  time: 'X-SRBUJ-TIME'
+                }
       #=> helpfull method to return redirect_to in an js way.
       # use: js_redirect(to: root_path)
       # use: js_redirect(reload: true)
@@ -24,11 +29,9 @@ module SrBuj
       # use: js_notify (message: 'this is madness!', type: 'error', side: 'right', position: 'top', time: 2000 )
       def js_notify(options = {})
         if options[:message]
-          response.headers['X-SRBUJ-MSG']  = options[:message].to_s
-          response.headers['X-SRBUJ-TYPE'] = options[:type].to_s      if options[:type]
-          response.headers['X-SRBUJ-SIDE'] = options[:side].to_s      if options[:side]
-          response.headers['X-SRBUJ-POS']  = options[:position].to_s  if options[:position]
-          response.headers['X-SRBUJ-TIME'] = options[:time].to_s      if options[:time]
+          HEADERS.each do |value, header|
+            response.headers[header] = options[value].to_s if options[value]
+          end
         end
       end
     end
