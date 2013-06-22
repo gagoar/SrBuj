04a4fdaabcd185e05333805aea61df0fc097bfdf
gagoar <xeroice@gmail.com>
2013-06-21 21:11:48 -0300

    generators for SrBuj js and css changing requires by a cleaner way to grow

--- lib/SrBuj.rb
+++ lib/SrBuj.rb
@@ -5,7 +5,8 @@ module SrBuj
   module Rails
     class Engine < ::Rails::Engine
       ActiveSupport.on_load(:action_controller) do
-        require 'SrBuj/rails/action_controller/helpers'
+        require 'SrBuj/rails/action_controller_helpers'
+        require 'SrBuj/rails/generators'
       end
     end
   end
