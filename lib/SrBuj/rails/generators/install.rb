990a1848455943ec813a6af335e19265e752215c
gagoar <xeroice@gmail.com>
2013-07-04 21:15:23 -0300

    fixing problem with stylesheets insertion

--- lib/SrBuj/rails/generators/install.rb
+++ lib/SrBuj/rails/generators/install.rb
@@ -5,10 +5,12 @@ module SrBuj
     class Install < Rails::Generators::Base
        desc "This generator installs SrBuj #{SrBuj::VERSION} in your manifests (js/css)"
 
+       AFTER_LIBRARY = {javascripts: 'jquery_ujs', stylesheets: 'require_self'}
+
       def add_assets
         [ detect_js_format, detect_css_format ].each do |format, prepend, type|
           say_status("inserting", "SrBuj (#{SrBuj::VERSION}) in your application#{format}", :green)
-          insert_into_file "app/assets/#{type}/application#{format}", "#{prepend} require SrBuj\n", after: "jquery_ujs\n"
+          insert_into_file "app/assets/#{type}/application#{format}", "\n#{prepend} require SrBuj", after: AFTER_LIBRARY[type.to_sym]
         end
       end
 
